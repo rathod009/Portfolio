b463fe2d462d3b8cf148c70e7f9bf99c60f81d24
rathod009 <bhumikrathod09@gmail.com>
2024-10-08 15:05:34 +0530

    Final commit: Updated Flutter Portfolio

--- lib/sections/portfolio/portfolio_mobile.dart
+++ lib/sections/portfolio/portfolio_mobile.dart
@@ -34,7 +34,7 @@ class PortfolioMobileTab extends StatelessWidget {
             ),
           ),
           options: CarouselOptions(
-            height: height * 0.30,
+            height: height * 0.28,
             autoPlay: true,
             autoPlayInterval: const Duration(seconds: 5),
             enlargeCenterPage: true,
