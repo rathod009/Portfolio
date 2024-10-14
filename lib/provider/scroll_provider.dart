3cade9290d59e92058af2eb6421555112d4ddc1f
rathod009 <bhumikrathod09@gmail.com>
2024-10-15 04:30:00 +0530

    Final commit: Updated Flutter Portfolio

--- lib/provider/scroll_provider.dart
+++ lib/provider/scroll_provider.dart
@@ -14,7 +14,7 @@ class ScrollProvider extends ChangeNotifier {
             : index == 3
                 ? 247
                 : index == 4
-                  ? 246
+                  ? 248
                   : 235;
     controller.animateTo(
       AppDimensions.normalize(
